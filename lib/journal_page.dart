1c170c933d3cf9d3e82258753970856cd0bb9cb4
Omar Toutounji <homieomt@hotmail.com>
2021-03-07 20:57:06 -0500

    added logo and dynamic cursor that changes colour based on phone's theme

--- lib/journal_page.dart
+++ lib/journal_page.dart
@@ -6,24 +6,34 @@ class JournalPage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Expanded(
-                child: TextField(
-                  keyboardType: TextInputType.text,
-                  maxLines: null,
-                  decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: 'how was your day?',
-                      hintStyle: TextStyle(fontSize: 30)
-                  ),
-                  showCursor: true,
-                  style: TextStyle(fontSize: 90),
-                  textAlign: TextAlign.center,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset("assets/hwyd_logo.png", width: 50)
+                  ],
                 ),
-              )
-            ],
+                Expanded(
+                  child: TextField(
+                    keyboardType: TextInputType.text,
+                    maxLines: null,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'how was your day?',
+                        hintStyle: TextStyle(fontSize: 40)
+                    ),
+                    showCursor: true,
+                    cursorColor: MediaQuery.of(context).platformBrightness == Brightness.dark ? Colors.white : Colors.black,
+                    style: TextStyle(fontSize: 90),
+                    textAlign: TextAlign.center,
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
